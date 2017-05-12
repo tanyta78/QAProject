62b5514dd62cf68991c551e31a0cdeaf0df52922
tanyta78 <tanyta78@gmail.com>
2017-05-13 00:09:40 +0300

    Resolved previous problem with SpecFlow

--- NewTestProject/LoginTestClass.cs
+++ NewTestProject/LoginTestClass.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NewTestProject.Models;
+using NewTestProject.Pages.LoginPage;
+using NewTestProject.Pages.RegisterPage;
 using NUnit.Framework;
 using OpenQA.Selenium;
 
